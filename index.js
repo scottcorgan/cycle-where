91060e6ef3a9a6208e0bb04a8c8f9ae1c4e30eef
Scott Corgan <scottcorgan@gmail.com>
2016-02-16 09:49:20 -0500

    Pull out route-params into module

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 'use strict'
 
 let Rx = require('rx')
-let pathnameMatch = require('pathname-match')
-let pathToRegexp = require('path-to-regexp')
-let memoize = require('mem')
 let urlJoin = require('url-join')
-let zipObject = require('zip-object')
+let routeParams = require('route-params')
 
 let Observable = Rx.Observable
 let ReplaySubject = Rx. ReplaySubject
@@ -14,18 +11,6 @@ const BACK = 'BACK'
 const FORWARD = 'FORWARD'
 const REDIRECT = 'REDIRECT'
 
-let getParams = memoize((route, originalPath) => {
-
-    let pathDetails = []
-    let pathRegexp = pathToRegexp(route, pathDetails)
-    let regexpResults = pathRegexp.exec(pathnameMatch(originalPath))
-    let paramNames = pathDetails.map(detail => detail.name)
-
-    return !!regexpResults
-        ? zipObject(paramNames, regexpResults.slice(1))
-        : undefined
-})
-let matchesRoute = memoize((r, pathname) => !!getParams(r, pathname))
 let isObservable = val => (typeof val === 'object' && typeof val.subscribe === 'function')
 
 exports.makeRouterDriver = function makeRouterDriver (history) {
@@ -74,13 +59,13 @@ exports.makeRouterDriver = function makeRouterDriver (history) {
 function makeRoute (source$, baseRoutePath, routePath) {
 
   let fullRoutePath = urlJoin(baseRoutePath, routePath)
-  let location$ = source$.filter(location => matchesRoute(fullRoutePath, location.pathname))
-  let params$ = location$.map(location => getParams(fullRoutePath, location.pathname))
+  let location$ = source$.filter(location => routeParams(fullRoutePath, location.pathname))
+  let params$ = location$.map(location => routeParams(fullRoutePath, location.pathname))
 
   function route (nextRoutePath) {
 
     return makeRoute(
-      source$.filter(location => matchesRoute(`${baseRoutePath}*:next`, location.pathname)),
+      source$.filter(location => routeParams(`${baseRoutePath}*:next`, location.pathname)),
       urlJoin(baseRoutePath, routePath),
       nextRoutePath
     )
